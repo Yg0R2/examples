ca5c6b47a1e1ed74a5cc552bed1df826b9462961
Yg0R2 <yg0r2@yahoo.com>
2020-09-01 13:18:47 +0200

    Force application to set session cookie in case of https and disable browser scripts to access it

---
+++ auth/src/main/java/yg0r2/examples/auth/ApplicationInitializer.java
@@ -0,0 +1,18 @@
+package yg0r2.examples.auth;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@Component
+public class ApplicationInitializer implements ServletContextInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.getSessionCookieConfig().setHttpOnly(true); // if true then browser script won't be able to access the cookie
+        servletContext.getSessionCookieConfig().setSecure(true); // if true then the cookie will be sent only over HTTPS connection
+    }
+
+}
