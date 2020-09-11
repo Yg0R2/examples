76bdd5a7d2ceb238e63cbdf0f5172cbde0d3491e
Yg0R2 <yg0r2@yahoo.com>
2020-09-11 23:39:02 +0200

    Add project dependencies

--- gradle-plugin/src/main/kotlin/yg0r2/examples/MyPlugin.kt
+++ gradle-plugin/src/main/kotlin/yg0r2/examples/MyPlugin.kt
@@ -4,7 +4,9 @@ import io.spring.gradle.dependencymanagement.DependencyManagementPlugin
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencySet
 import org.gradle.api.plugins.JavaPlugin
+import org.gradle.kotlin.dsl.add
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 import yg0r2.examples.generate.FileDataExtension
 import yg0r2.examples.generate.GenerateTask
@@ -23,7 +25,17 @@ class MyPlugin : Plugin<Project> {
         }
         project.plugins.withType(JavaPlugin::class.java, javaPluginAction)
 
-        //project.dependencies.add()
+        // Add project dependencies
+        val configuration = project.configurations.getByName("implementation")
+        configuration.defaultDependencies {
+            addAll(listOf(
+                project.dependencies.create("org.springframework.boot:spring-boot-starter-data-redis"),
+                project.dependencies.create("org.springframework.boot:spring-boot-starter-security"),
+                project.dependencies.create("org.springframework.boot:spring-boot-starter-thymeleaf"),
+                project.dependencies.create("org.springframework.boot:spring-boot-starter-web"),
+                project.dependencies.create("org.springframework.session:spring-session-data-redis")
+            ))
+        }
 
         val fileDataExtension = project.extensions.create("fileData", FileDataExtension::class.java, project)
         project.tasks.register("generate", GenerateTask::class.java) {
