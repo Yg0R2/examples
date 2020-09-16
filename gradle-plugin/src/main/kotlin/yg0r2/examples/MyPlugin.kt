361f17139ba4998ca75a9a0893c0ca3670f46e68
Yg0R2 <yg0r2@yahoo.com>
2020-09-16 20:59:02 +0200

    Use gradle plugin to define project structure

--- gradle-plugin/src/main/kotlin/yg0r2/examples/MyPlugin.kt
+++ gradle-plugin/src/main/kotlin/yg0r2/examples/MyPlugin.kt
@@ -7,16 +7,20 @@ import org.gradle.api.Action
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaLibraryPlugin
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.exclude
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 import yg0r2.examples.generate.FileDataExtension
 import yg0r2.examples.generate.GenerateTask
 import yg0r2.examples.model.Dependency
-import java.lang.IllegalArgumentException
+import java.util.*
 
 class MyPlugin : Plugin<Project> {
 
@@ -29,6 +33,7 @@ class MyPlugin : Plugin<Project> {
                 it.plugins.apply {
                     apply(DependencyManagementPlugin::class.java)
                     apply(JavaPlugin::class.java)
+                    apply(JavaLibraryPlugin::class.java)
                     apply(SpringBootPlugin::class.java)
                 }
 
@@ -39,6 +44,74 @@ class MyPlugin : Plugin<Project> {
             }
         }
 
+        project.subprojects.forEach {
+            it.tasks.named("bootJar", BootJar::class.java) {
+                enabled = false
+            }
+
+            it.tasks.named("jar", Jar::class.java) {
+                enabled = true
+            }
+        }
+
+        val apiProject = getSubProject(project.subprojects, "api")
+        apiProject.ifPresent {
+            getDependencySet(it, "api").apply {
+                add(it.dependencies.create("javax.validation", "validation-api", "+"))
+            }
+
+            getDependencySet(it).apply {
+                add(it.dependencies.create("com.fasterxml.jackson.core", "jackson-annotations", "+"))
+                add(it.dependencies.create("com.fasterxml.jackson.core", "jackson-databind", "+"))
+                add(it.dependencies.create("org.apache.commons", "commons-lang3", "+"))
+            }
+        }
+
+        val clientProject = getSubProject(project.subprojects, "client")
+        clientProject.ifPresent {
+            getDependencySet(it, "api").apply {
+                apiProject.ifPresent { project -> add(it.dependencies.create(project)) }
+            }
+        }
+
+        val daoProject = getSubProject(project.subprojects, "dao")
+        daoProject.ifPresent {
+            getDependencySet(it, "api").apply {
+                add(it.dependencies.create("org.springframework.boot", "spring-boot-starter-data-jpa"))
+            }
+
+            getDependencySet(it).apply {
+                add(it.dependencies.create("com.h2database", "h2"))
+                add(it.dependencies.create("org.apache.commons", "commons-lang3", "+"))
+                add(it.dependencies.create("org.springframework", "spring-context"))
+            }
+        }
+
+        val serviceProject = getSubProject(project.subprojects, "service")
+        serviceProject.ifPresent {
+            getDependencySet(it, "api").apply {
+                apiProject.ifPresent { project -> add(it.dependencies.create(project)) }
+                daoProject.ifPresent { project -> add(it.dependencies.create(project)) }
+            }
+
+            getDependencySet(it).apply {
+                add(it.dependencies.create("org.springframework.security", "spring-security-core"))
+            }
+        }
+
+        val webProject = getSubProject(project.subprojects, "web")
+        webProject.ifPresent {
+            getDependencySet(it, "api").apply {
+                serviceProject.ifPresent { project -> add(it.dependencies.create(project)) }
+
+                add(it.dependencies.create("org.springframework.boot", "spring-boot-starter-data-redis"))
+                add(it.dependencies.create("org.springframework.boot", "spring-boot-starter-security"))
+                add(it.dependencies.create("org.springframework.boot", "spring-boot-starter-thymeleaf"))
+                add(it.dependencies.create("org.springframework.boot", "spring-boot-starter-web"))
+                add(it.dependencies.create("org.springframework.session", "spring-session-data-redis"))
+            }
+        }
+
         // Execute in case of JavaPlugin present
         val javaPluginAction = Action<JavaPlugin> {
             println("Jee JavaPlugin found :)")
@@ -46,18 +119,13 @@ class MyPlugin : Plugin<Project> {
         project.plugins.withType(JavaPlugin::class.java, javaPluginAction)
 
         // Add project dependencies
-        val configuration = project.configurations.getByName("implementation")
-        configuration.dependencies.addAll(listOf(
-            project.dependencies.create("org.springframework.boot:spring-boot-starter-data-redis"),
-            project.dependencies.create("org.springframework.boot:spring-boot-starter-security"),
-            project.dependencies.create("org.springframework.boot:spring-boot-starter-thymeleaf"),
-            project.dependencies.create("org.springframework.boot:spring-boot-starter-web"),
-            project.dependencies.create("org.springframework.session:spring-session-data-redis")
-        ))
+        getDependencySet(project).apply {
+            webProject.ifPresent { project -> add(project.dependencies.create(project)) }
+        }
 
-        project.configurations.getByName("testImplementation").dependencies.addAll(listOf(
-            project.dependencies.create("org.springframework.boot:spring-boot-starter-test")
-        ))
+        getDependencySet(project, "testImplementation").apply {
+            add(project.dependencies.create("org.springframework.boot:spring-boot-starter-test"))
+        }
 
         // Excluding dependency https://docs.gradle.org/current/userguide/resolution_rules.html
         val excludedDependencies = readJsonResource<List<Dependency>>("excludedDependencies.json")
@@ -99,6 +167,14 @@ class MyPlugin : Plugin<Project> {
         val url = javaClass.classLoader.getResource(resourcePath) ?: throw IllegalArgumentException("Resource does not exist: $resourcePath")
 
         return Json.decodeFromString(url.readText())
+    }
+
+    private fun getDependencySet(it: Project, name: String = "implementation") = it.configurations.getByName(name).dependencies
+
+    private fun getSubProject(subProjects: Set<Project>, name: String): Optional<Project> {
+        return subProjects.stream()
+            .filter { it.name == name }
+            .findFirst()
     }
 
 }
