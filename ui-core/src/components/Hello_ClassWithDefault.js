f27b7c2c7a79f2920a0a7b96a41e7071e0aac912
Yg0R2 <yg0r2@yahoo.com>
2021-02-02 07:56:17 +0100

    UI - Replace UI-Core with ReactLibrary

--- ui-core/src/components/Hello_ClassWithDefault.js
+++
@@ -1,9 +0,0 @@
-export default class Hello_ClassWithDefault {
-  helloClassWithDefault = (name = 'World') => {
-    console.log(`Hello ${name}`);
-  }
-
-  static staticHelloClassWithDefault = (name = 'World') => {
-    console.log(`Hello ${name}`);
-  }
-}
---
+++ ui-core/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+import ExampleComponent from './components/example/ExampleComponent'
+import Form from './components/form/Form'
+
+const Core = {
+  ExampleComponent,
+  Form
+}
+export default Core
