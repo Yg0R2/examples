4dd3b5e19f3dd9ff2a55690429bf0e479f4c16e4
Yg0R2 <yg0r2@yahoo.com>
2021-09-02 14:41:09 +0200

    fix aggregator goal

--- maven-extension-plugin/plugin/src/main/kotlin/yg0r2/examples/maven/plugin/AggregateMojo.kt
+++ maven-extension-plugin/plugin/src/main/kotlin/yg0r2/examples/maven/plugin/AggregateMojo.kt
@@ -1,14 +1,10 @@
 package yg0r2.examples.maven.plugin
 
-import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugins.annotations.Execute
-import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
-import org.codehaus.plexus.util.cli.CommandLineUtils
-import org.codehaus.plexus.util.cli.Commandline
 
 @Mojo(aggregator = true, name = "aggregate", threadSafe = true)
-@Execute(phase = LifecyclePhase.COMPILE)
+@Execute(goal = "examples")
 class AggregateMojo: AbstractExamplesMojo() {
 
     override fun execute() {
@@ -17,23 +13,6 @@ class AggregateMojo: AbstractExamplesMojo() {
 
             return
         }
-
-        val cmd = Commandline()
-        cmd.executable = "mvn"
-        cmd.addArguments(arrayOf("examples:examples"))
-
-        cmd.workingDirectory = project.basedir
-
-        val out = CommandLineUtils.StringStreamConsumer()
-        val error = CommandLineUtils.StringStreamConsumer()
-
-        val exitCode = CommandLineUtils.executeCommandLine(cmd, out, error)
-
-        if (exitCode != 0) {
-            throw MojoExecutionException(out.output)
-        }
-
-        log.info(out.output)
     }
 
 }
