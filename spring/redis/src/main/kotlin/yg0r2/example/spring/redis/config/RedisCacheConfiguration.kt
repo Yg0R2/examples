2be46e5ae10f82f0f920a36bfb9db7669798c3a2
Yg0R2 <yg0r2@yahoo.com>
2022-10-06 10:34:19 +0200

    spring-redis: use local redis

--- spring/redis/src/main/kotlin/yg0r2/example/spring/redis/config/RedisCacheConfiguration.kt
+++ spring/redis/src/main/kotlin/yg0r2/example/spring/redis/config/RedisCacheConfiguration.kt
@@ -1,15 +1,23 @@
 package yg0r2.example.spring.redis.config
 
-import org.springframework.cache.CacheManager
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import java.time.Duration
 
 @Configuration
 class RedisCacheConfiguration {
 
     @Bean
-    fun cacheManager(): CacheManager =
-        ConcurrentMapCacheManager()
+    fun redisCacheManagerBuilderCustomizer(): RedisCacheManagerBuilderCustomizer =
+        RedisCacheManagerBuilderCustomizer { builder ->
+            builder.withCacheConfiguration(
+                "dummy-cache",
+                RedisCacheConfiguration.defaultCacheConfig()
+                    .disableCachingNullValues()
+                    .entryTtl(Duration.ofSeconds(5))
+            )
+        }
 
 }
